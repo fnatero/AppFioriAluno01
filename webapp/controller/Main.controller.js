3107db33289f8afa6bfa44c89431243d5d1d9937
Felipe N. Gimenez <fnatero@gmail.com>
2021-12-17 00:46:07 +0000

    first commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("AppFioriAluno01.appfiorialuno01.controller.Main", {
+            onInit: function () {
+
+            }
+        });
+    });
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"AppFioriAluno01/appfiorialuno01/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
